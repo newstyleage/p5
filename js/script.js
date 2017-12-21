bdae6e200d1cb15153e296a319da10316658d79c
NS <newstyleage@gmail.com>
2017-12-21 12:19:41 +0100

    new branch

--- js/script.js
+++ js/script.js
@@ -1,20 +1,14 @@
+let x = 0;
 
 function setup() {
   // put setup code here
   createCanvas(600, 400);
-  noCursor();
+  // noCursor();
 }
 
 function draw() {
   // put drawing code here
   background(200);
 
-  let shadowX = map(mouseX, 0, width, -30, 30);
-  let shadowY = map(mouseY, 0, height, -30, 30);
-  drawingContext.shadowOffsetX = shadowX;
-  drawingContext.shadowOffsetY = shadowY;
-  drawingContext.shadowBlur = 30;
-  drawingContext.shadowColor = "rgba(100, 100, 100, 100)";
 
-  ellipse(mouseX, mouseY, 50, 50);
 }
