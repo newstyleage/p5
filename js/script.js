d714b50223d8bf59549a9cd6d45767ba01b1dce1
NS <newstyleage@gmail.com>
2017-12-21 11:48:51 +0100

    shadow with mousemove

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
+
 function setup() {
   // put setup code here
   createCanvas(600, 400);
+  noCursor();
 }
 
 function draw() {
   // put drawing code here
   background(200);
-  rect(100, 100, 100, 100);
+
+  let shadowX = map(mouseX, 0, width, -30, 30);
+  let shadowY = map(mouseY, 0, height, -30, 30);
+  drawingContext.shadowOffsetX = shadowX;
+  drawingContext.shadowOffsetY = shadowY;
+  drawingContext.shadowBlur = 30;
+  drawingContext.shadowColor = "rgba(100, 100, 100, 100)";
+
+  ellipse(mouseX, mouseY, 50, 50);
 }
