582e8f17156d9a7d311b564439d1d1be058c307a
NS <newstyleage@gmail.com>
2017-12-21 17:24:59 +0100

    functions arguments parameters

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 let ball = {
   x: 300,
   y: 200,
-  xSpd: 4,
-  ySpd: -3
+  xSpd: 2,
+  ySpd: -4
 }
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(800, 500);
 
 }
 function draw() {
   background(0);
-  move();
-  bounce();
-  display();
+  drawRobot(120, 30, 110, 140);
+  drawRobot(270, 60, 260, 95);
+  drawRobot(420, 120, 80, 30);
+  drawRobot(570, 90, 180, 50);
 }
 
-function move() {
-  ball.x += ball.xSpd;
-  ball.y += ball.ySpd;
-}
+function drawRobot(x, y, b, n) {
+  fill(255, 255, 200);
+  rect(x, y, 10, n);
+  fill(255, 200, 40);
+  ellipse(x+5, y, 40, 40);
+  fill(255, 200, 0);
+  ellipse(x, y-10, 10, 10);
+  ellipse(x+10, y-10, 10, 10);
 
-function bounce() {
-  if (ball.x > width || ball.x <0) {
-    ball.xSpd = -ball.xSpd;
-  }
-  if (ball.y > height || ball.y <0) {
-    ball.ySpd = -ball.ySpd;
-  }
-}
-
-function display() {
-  stroke(255);
-  strokeWeight(4);
-  noFill();
-  ellipse(ball.x, ball.y, 24, 24);
+  stroke(255, 255, 200);
+ strokeWeight(10);
+  line(x-9,y+n+10, x-59, y+n+50);
+  line(x-9,y+n+b, x-99, y+n+b+50);
+  line(x+19,y+n+10, x+59, y+n+50);
+  line(x+19,y+n+b, x+99, y+n+b+50);
+  strokeWeight(1);
+  fill(255, 255, 12);
+  rect(x-9, y+n, 30, b);
 }
