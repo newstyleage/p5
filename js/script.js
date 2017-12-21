d30d8bbfee4a342eddecbd3927ce8cf1346bf511
NS <newstyleage@gmail.com>
2017-12-21 12:53:33 +0100

    nested loop

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
 
 function setup() {
   // put setup code here
-  createCanvas(600, 400);
-  // noCursor();
-  background(0);
-  strokeWeight(4);
-  stroke(200);
-  let x = 0;
-  while (x <= width) {
-    fill(255, 255, x*1.5-250);
-    ellipse(x, 100, 25, 25);
-    x += 50;
-  }
-  for (let x1=0; x1<=width; x1+=50){
-    fill(255, 255, x1/2);
-    ellipse(x1, 300, 25, 25);
+  createCanvas(600, 600);
+  background(255);
+
+  for (let x=0; x<=width; x+=15){
+    for ( let y=0; y<=height; y+=15){
+
+      fill(x-100, 255, y-100);
+      ellipse(x, y, 10, 10);
+    }
   }
 }
 
