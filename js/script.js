d8d60987daa744a0bedaa18b40629014b8b3a67e
NS <newstyleage@gmail.com>
2017-12-21 12:46:46 +0100

    loops

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-let x = 0;
 
 function setup() {
   // put setup code here
   createCanvas(600, 400);
   // noCursor();
+  background(0);
+  strokeWeight(4);
+  stroke(200);
+  let x = 0;
+  while (x <= width) {
+    fill(255, 255, x*1.5-250);
+    ellipse(x, 100, 25, 25);
+    x += 50;
+  }
+  for (let x1=0; x1<=width; x1+=50){
+    fill(255, 255, x1/2);
+    ellipse(x1, 300, 25, 25);
+  }
 }
 
 function draw() {
   // put drawing code here
-  background(0);
-
-
 }
