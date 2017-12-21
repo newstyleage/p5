4c9bf344c6b0fa0e09d12758c4e10bf6a4068b44
NS <newstyleage@gmail.com>
2017-12-21 13:12:29 +0100

    function basics

--- js/script.js
+++ js/script.js
@@ -5,23 +5,34 @@ let ball = {
   ySpd: -3
 }
 
-
 function setup() {
   createCanvas(600, 400);
 
 }
 function draw() {
   background(0);
-  stroke(255);
-  strokeWeight(4);
-  noFill();
-  ellipse(ball.x, ball.y, 24, 24);
+  move();
+  bounce();
+  display();
+}
+
+function move() {
+  ball.x += ball.xSpd;
+  ball.y += ball.ySpd;
+}
+
+function bounce() {
   if (ball.x > width || ball.x <0) {
     ball.xSpd = -ball.xSpd;
   }
   if (ball.y > height || ball.y <0) {
     ball.ySpd = -ball.ySpd;
   }
-  ball.x += ball.xSpd;
-  ball.y += ball.ySpd;
+}
+
+function display() {
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  ellipse(ball.x, ball.y, 24, 24);
 }
