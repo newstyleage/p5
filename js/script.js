8ce9797faa8cb08d98e5f42baf66cf1f7cde6234
NS <newstyleage@gmail.com>
2017-12-22 10:43:58 +0100

    Arrays

--- js/script.js
+++ js/script.js
@@ -1,41 +1,25 @@
-let bubble, bubble2;
-
+let color = "red";
+let words = ['kot', 'pies', 'zima', 'śnieg', 'góry'];
+const colors = ['aqua', 'blue', 'fuchsia', 'gray', 'green',
+'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red',
+'silver', 'teal', 'white', 'yellow'];
+let idx = 0;
 function setup() {
   createCanvas(600, 400);
-  bubble = new Bubble(200, 200, 40);
-  bubble2 = new Bubble(400, 200, 20);
+
 }
 
 function draw() {
   background(0);
-  bubble.show();
-  bubble.move();
-  bubble2.show();
-  bubble2.move();
-
+  fill(color);
+  textSize(32);
+  text(words[idx], 12, 200);
 }
 
-class Bubble {
-  constructor(x, y, r) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-  }
-
-  show() {
-    stroke(255);
-    strokeWeight(4);
-    noFill();
-    ellipse(this.x, this.y, this.r*2);
-  }
-
-  move() {
-    this.x += random(-5, 5);
-    this.y += random(-5, 5);
-
-    if(this.x<0 || this.x>width)
-      this.x = -this.x;
-    if(this.y<0 || this.y>height)
-      this.y = -this.y;
+function mousePressed() {
+  color=random(colors);
+  idx++;
+  if(idx == words.length){
+    idx = 0;
   }
 }
