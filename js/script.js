c35b89db44654c22426466eb6a6464dd7e9d3953
NS <newstyleage@gmail.com>
2017-12-22 08:13:31 +0100

    Classes

--- js/script.js
+++ js/script.js
@@ -1,38 +1,40 @@
-let ball = {
-  x: 300,
-  y: 200,
-  xSpd: 2,
-  ySpd: -4
-}
+let bubble, bubble2;
 
 function setup() {
-  createCanvas(800, 500);
-
+  createCanvas(600, 400);
+  bubble = new Bubble();
+  bubble2 = new Bubble();
 }
+
 function draw() {
   background(0);
-  drawRobot(120, 30, 110, 140);
-  drawRobot(270, 60, 260, 95);
-  drawRobot(420, 120, 80, 30);
-  drawRobot(570, 90, 180, 50);
+  bubble.show();
+  bubble.move();
+  bubble2.show();
+  bubble2.move();
+
 }
 
-function drawRobot(x, y, b, n) {
-  fill(255, 255, 200);
-  rect(x, y, 10, n);
-  fill(255, 200, 40);
-  ellipse(x+5, y, 40, 40);
-  fill(255, 200, 0);
-  ellipse(x, y-10, 10, 10);
-  ellipse(x+10, y-10, 10, 10);
+class Bubble {
+  constructor() {
+    this.x = random(width);
+    this.y = random(height);
+  }
 
-  stroke(255, 255, 200);
- strokeWeight(10);
-  line(x-9,y+n+10, x-59, y+n+50);
-  line(x-9,y+n+b, x-99, y+n+b+50);
-  line(x+19,y+n+10, x+59, y+n+50);
-  line(x+19,y+n+b, x+99, y+n+b+50);
-  strokeWeight(1);
-  fill(255, 255, 12);
-  rect(x-9, y+n, 30, b);
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, 24, 24);
+  }
+
+  move() {
+    this.x += random(-5, 5);
+    this.y += random(-5, 5);
+    
+    if(this.x<0 || this.x>width)
+      this.x = -this.x;
+    if(this.y<0 || this.y>height)
+      this.y = -this.y;
+  }
 }
