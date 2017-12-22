8cd9de65ea3f2bbfb370afd7f5a668b9b66b97ed
NS <newstyleage@gmail.com>
2017-12-22 10:58:01 +0100

    Array loop

--- js/script.js
+++ js/script.js
@@ -1,25 +1,12 @@
-let color = "red";
-let words = ['kot', 'pies', 'zima', 'śnieg', 'góry'];
-const colors = ['aqua', 'blue', 'fuchsia', 'gray', 'green',
-'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red',
-'silver', 'teal', 'white', 'yellow'];
-let idx = 0;
+const nums = [100, 25, 46, 72];
+
 function setup() {
   createCanvas(600, 400);
-
 }
 
 function draw() {
   background(0);
-  fill(color);
-  textSize(32);
-  text(words[idx], 12, 200);
-}
-
-function mousePressed() {
-  color=random(colors);
-  idx++;
-  if(idx == words.length){
-    idx = 0;
-  }
+  fill(111);
+  for(i=0;i<nums.length;i++)
+    ellipse(100*i+100, 200, nums[i]);
 }
