0656ba3fb0fb6e19c57da858b73cfabfb89ea103
NS <newstyleage@gmail.com>
2017-12-22 21:50:11 +0100

    btn callback

--- js/script.js
+++ js/script.js
@@ -1,21 +1,19 @@
-var canvas;
-var h1;
+var bg, btn;
+
 
 function setup() {
   canvas = createCanvas(200, 200);
-  canvas.position(400, 100);
-  h1 = createElement('h1', 'Waiting...');
-  //h1.position(400, 100);
+  bg = color(200);
+  btn = createButton("go!");
+  btn.mousePressed(changeColor);
 }
 
-function mousePressed() {
-  h1.html("Now I'll show you my fav nums!")
-  createP('May fav number is ' + random(0, 10));
+function changeColor(){
+  bg = color(random(255));
 }
 
 function draw() {
-  //clear();
-  background(200);
+  background(bg);
   fill(255, 0, 100);
   rect(100, 100, 50, 50);
 }
