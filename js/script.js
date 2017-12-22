fe575e35a7afc45dfad899ea7fb364590763994f
NS <newstyleage@gmail.com>
2017-12-22 22:34:09 +0100

    8.6: Other Events and Inputs

--- js/script.js
+++ js/script.js
@@ -5,21 +5,37 @@ function setup() {
   canvas = createCanvas(200, 200);
   bg = color(200);
   nameP = createP("Your name");
+  nameP.mouseOver(overPara);
+  nameP.mouseOut(outPara);
   btn = createButton("go!");
   btn.mousePressed(changeColor);
 
   slider = createSlider(10, 100, 47);
   input = createInput('type your name');
+  input.changed(updateText);
 }
 
 function changeColor(){
   bg = color(random(255));
 }
 
+function overPara(){
+  nameP.html('your mouse is over me')
+}
+
+function outPara(){
+
+  nameP.html('your mouse is OUT')
+}
+
+function updateText(){
+  nameP.html(input.value());
+}
+
 function draw() {
   background(bg);
   fill(255, 0, 100);
   ellipse(100, 100, slider.value(), slider.value());
-  nameP.html(input.value());
+  //nameP.html(input.value());
   text(input.value(), 10, 20);
 }
