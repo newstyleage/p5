0487f09a33fa8a6e01775ab009d49c66bc200842
NS <newstyleage@gmail.com>
2017-12-22 22:07:40 +0100

    Sliders, Buttons and Text Inputs

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-var bg, btn;
+var bg, btn, slider, input, nameP;
 
 
 function setup() {
   canvas = createCanvas(200, 200);
   bg = color(200);
+  nameP = createP("Your name");
   btn = createButton("go!");
   btn.mousePressed(changeColor);
+
+  slider = createSlider(10, 100, 47);
+  input = createInput('type your name');
 }
 
 function changeColor(){
@@ -15,5 +19,7 @@ function changeColor(){
 function draw() {
   background(bg);
   fill(255, 0, 100);
-  rect(100, 100, 50, 50);
+  ellipse(100, 100, slider.value(), slider.value());
+  nameP.html(input.value());
+  text(input.value(), 10, 20);
 }
