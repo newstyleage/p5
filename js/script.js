341b50125b2a8a9f96b8f015ec6a646bd185d721
NS <newstyleage@gmail.com>
2017-12-22 11:20:04 +0100

    Array of objects

--- js/script.js
+++ js/script.js
@@ -1,12 +1,44 @@
-const nums = [100, 25, 46, 72];
+let bubbles = [];
 
 function setup() {
   createCanvas(600, 400);
+  for(i=0;i<10;i++){
+    let x= random(width);
+    let y = random(height);
+    let r = random(20, 40);
+    bubbles[i] = new Bubble(x, y, r);
+  }
 }
 
 function draw() {
   background(0);
-  fill(111);
-  for(i=0;i<nums.length;i++)
-    ellipse(100*i+100, 200, nums[i]);
+  for (i = 0; i < bubbles.length; i++) {
+    bubbles[i].show();
+    bubbles[i].move();
+  }
+}
+
+class Bubble {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, this.r * 2);
+  }
+
+  move() {
+    if (this.x < this.r || this.x > (width -  this.r))
+    this.x = -this.x;
+    if (this.y < this.r || this.y > (height - this.r))
+    this.y = -this.y;
+
+    this.x += random(-5, 5);
+    this.y += random(-5, 5);
+  }
 }
