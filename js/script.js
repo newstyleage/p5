62c1b5dfc7d282e5e98adb450534e56bfc2baab5
NS <newstyleage@gmail.com>
2017-12-23 22:16:29 +0100

    object trails 1

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
-const gravity = 0.1;
-
 function Particle(x, y) {
   this.x = x;
   this.y = y;
 
-  this.ySpeed = 0;
+  this.history = [];
 
   this.update = function() {
-    this.y += this.ySpeed;
-    this.ySpeed += gravity;
+    print(this.x);
+    print(this.y);
+    this.x += random(-5, 5);
+    this.y += random(-5, 5);
 
-    if (this.y > height) {
-      this.y = height;
-      this.ySpeed *= -0.9;
-    }
+    let v = createVector(this.x, this.y);
+    this.history.push(v);
+
   }
 
   this.show = function() {
     stroke(0);
     fill(0, 150);
     ellipse(this.x, this.y, 24, 24);
+
+    for (let i = 0; i < this.history.length; i++) {
+      let pos = this.history[i];
+      ellipse(pos.x, pos.y, 4, 4);
+    }
   }
 }
 
@@ -27,11 +31,12 @@ let particle;
 
 function setup() {
   createCanvas(400, 300);
-  particle = new Particle(100, 100);
+  particle = new Particle(200, 200);
 
 }
 function draw() {
   background(200);
   particle.update();
   particle.show();
+
 }
