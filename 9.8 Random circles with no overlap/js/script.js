7d98738d3bdfbdc6b1590499cd38a4cb99653cbe
NS <newstyleage@gmail.com>
2017-12-27 13:18:56 +0100

    dangerous while!

--- 9.8 Random circles with no overlap/js/script.js
+++ 9.8 Random circles with no overlap/js/script.js
@@ -1,14 +1,34 @@
+let circles = [];
+
 function setup() {
   createCanvas(640 , 360);
+while (circles.length < 25){
+  //for (let i = 0; i < 25; i++) {
 
-  for (let i = 0; i < 50; i++) {
-    let x = random(width);
-    let y = random(height);
-    let r = 48;
-    fill(255, 255, 100, 100);
-    noStroke();
-    ellipse(x, y, 2 * r, 2 * r);
+    let circle = {
+      x: random(width),
+      y: random(height),
+      r: 32
+    }
+    let overlapping = false;
+    for (j=0; j<circles.length; j++) {
+      let other = circles[j];
+      let d = dist(circle.x, circle.y, other.x, other.y);
+      if (d < circle.r + other.r) {
+        overlapping = true;
+        break;
+      }
+    }
+    if (!overlapping){
+      circles.push(circle);
+    }
+
   }
+    for (let i = 0; i < circles.length; i++) {
+    fill(255, 255, 100, 100);
+      noStroke();
+      ellipse(circles[i].x, circles[i].y, 2 * circles[i].r, 2 * circles[i].r);
+    }
 }
 
 function draw() {
---
+++ js/particles.js
@@ -0,0 +1,37 @@
+function Particle(x, y) {
+  this.x = x;
+  this.y = y;
+
+  this.history = [];
+
+  this.update = function() {
+    this.x += random(-10, 10);
+    this.y += random(-10, 10);
+
+    for (let i = 0; i < this.history.length; i++) {
+      this.history[i].x += random(-2, 2);
+      this.history[i].y += random(-2, 2);
+    }
+
+    let v = createVector(this.x, this.y);
+    this.history.push(v);
+    if (this.history.length > 70) {
+      this.history.splice(0, 1);
+    }
+
+  }
+
+  this.show = function() {
+    stroke(0);
+    fill(0, 150);
+    ellipse(this.x, this.y, 24, 24);
+    noFill();
+    beginShape();
+    for (let i = 0; i < this.history.length; i++) {
+      let pos = this.history[i];
+      // ellipse(pos.x, pos.y, i/5, i/5);
+      vertex(pos.x, pos.y);
+    }
+    endShape();
+  }
+}
