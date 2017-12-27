92dd97393f51cb4d5d3650cfaaf0122e6a68592e
NS <newstyleage@gmail.com>
2017-12-27 20:18:05 +0100

    curveVertex

--- 9.22 p5.js custom shapes/js/script.js
+++ 9.22 p5.js custom shapes/js/script.js
@@ -7,13 +7,20 @@ function draw() {
   background(51);
   stroke(255, 255, 100);
   noFill();
+  strokeWeight(4);
+  point(mouseX, mouseY);
+  point(150, 50);
+  point(250, 60);
+  point(300, 200);
+  strokeWeight(1);
   beginShape();
-  let spacing = map(mouseX, 0, width, 5, 100);
-  for (let a = 0 ; a < 360; a +=spacing){
-    let x = 100*sin(a)+200;
-    let y = 100*cos(a)+200;
-    vertex(x,y);
-  }
+
+    curveVertex(mouseX,mouseY);
+    curveVertex(150,50);
+    curveVertex(250,60);
+    curveVertex(300,200);
+
+
 
   endShape(CLOSE);
 }
