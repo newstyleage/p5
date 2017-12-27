332d92b534399f488e3d76869fb1c8a4722b23bd
NS <newstyleage@gmail.com>
2017-12-27 20:08:18 +0100

    custom shapes

--- 9.22 p5.js custom shapes/js/script.js
+++ 9.22 p5.js custom shapes/js/script.js
@@ -1,61 +1,19 @@
-let vertices = [];
-
 function setup() {
-  createCanvas(640, 360);
-
-  for (let i = 0; i < 20; i++) {
-    var v = createVector(random(width), random(height));
-    vertices.push(v);
-  }
-}
-
-function mousePressed() {
-  let v = createVector(mouseX, mouseY);
-  vertices.push(v);
+  createCanvas(480, 360);
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(51);
-  let reached = [];
-  let unreached = [];
-  for (let i = 0;i < vertices.length; i++) {
-    unreached.push(vertices[i]);
+  stroke(255, 255, 100);
+  noFill();
+  beginShape();
+  let spacing = map(mouseX, 0, width, 5, 100);
+  for (let a = 0 ; a < 360; a +=spacing){
+    let x = 100*sin(a)+200;
+    let y = 100*cos(a)+200;
+    vertex(x,y);
   }
 
-  reached.push(unreached[0]);
-  unreached.splice(0,1);
-
-  while (unreached.length > 0) {
-    let record = 100000;
-    let rIndex;
-    let uIndex;
-    for (let i=0; i < reached.length; i++) {
-      
-      for (let j = 0; j < unreached.length; j++){
-        let v1 = reached[i];
-        let v2 = unreached[j];
-        let d = dist(v1.x, v1.y, v2.x, v2.y);
-
-        if (d < record) {
-          record = d;
-          rIndex = i;
-          uIndex = j;
-        }
-      }
-    }
-    stroke(255, 255, 200);
-    strokeWeight(2);
-    line(reached[rIndex].x, reached[rIndex].y, unreached[uIndex].x, unreached[uIndex].y);
-
-    reached.push(unreached[uIndex]);
-    unreached.splice(uIndex,1);
-
-
-  }
-
-  for (let i = 0; i < vertices.length; i++) {
-    fill(255, 255, 100);
-    stroke(255, 255, 200);
-    ellipse(vertices[i].x, vertices[i].y, 16, 16);
-  }
+  endShape(CLOSE);
 }
