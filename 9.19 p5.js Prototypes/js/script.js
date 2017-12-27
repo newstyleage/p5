5b0a21baae3f3e591665c8e4cde88c95a54cb0ab
NS <newstyleage@gmail.com>
2017-12-27 19:38:14 +0100

    prototypes

--- 9.19 p5.js Prototypes/js/script.js
+++ 9.19 p5.js Prototypes/js/script.js
@@ -1,61 +1,21 @@
-let vertices = [];
+function Particle() {
+  this.x = 100;
+  this.y = 99;
 
-function setup() {
-  createCanvas(640, 360);
-
-  for (let i = 0; i < 20; i++) {
-    var v = createVector(random(width), random(height));
-    vertices.push(v);
-  }
 }
 
-function mousePressed() {
-  let v = createVector(mouseX, mouseY);
-  vertices.push(v);
+Particle.prototype.show = function () {
+  point(this.x, this.y);
+}
+var p, p1;
+function setup() {
+  createCanvas(640, 360);
+  p = new Particle();
+  p1 = new Particle();
+
 }
 
 function draw() {
   background(51);
-  let reached = [];
-  let unreached = [];
-  for (let i = 0;i < vertices.length; i++) {
-    unreached.push(vertices[i]);
-  }
 
-  reached.push(unreached[0]);
-  unreached.splice(0,1);
-
-  while (unreached.length > 0) {
-    let record = 100000;
-    let rIndex;
-    let uIndex;
-    for (let i=0; i < reached.length; i++) {
-      
-      for (let j = 0; j < unreached.length; j++){
-        let v1 = reached[i];
-        let v2 = unreached[j];
-        let d = dist(v1.x, v1.y, v2.x, v2.y);
-
-        if (d < record) {
-          record = d;
-          rIndex = i;
-          uIndex = j;
-        }
-      }
-    }
-    stroke(255, 255, 200);
-    strokeWeight(2);
-    line(reached[rIndex].x, reached[rIndex].y, unreached[uIndex].x, unreached[uIndex].y);
-
-    reached.push(unreached[uIndex]);
-    unreached.splice(uIndex,1);
-
-
-  }
-
-  for (let i = 0; i < vertices.length; i++) {
-    fill(255, 255, 100);
-    stroke(255, 255, 200);
-    ellipse(vertices[i].x, vertices[i].y, 16, 16);
-  }
 }
