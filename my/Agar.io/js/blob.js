7d29593d975ca99570b8200fc61fd19bc67ecfa5
NS <newstyleage@gmail.com>
2017-12-29 19:24:40 +0100

    borders, grow limit, random color(one)

--- my/Agar.io/js/blob.js
+++ my/Agar.io/js/blob.js
@@ -1,21 +1,25 @@
 function Blob(x, y, r) {
     this.pos = createVector(x, y);
     this.r = r;
+
     this.vel = createVector(0, 0);
     this.update = function(){
         var newVel = createVector(mouseX-width/2, mouseY-width/2);
         newVel.setMag(3);
         this.vel.lerp(newVel, 0.2);
         this.pos.add(this.vel);
+        if(this.pos.x<-width){this.pos.x = -width}
+        if(this.pos.y<-height){this.pos.y = -height}
+        if(this.pos.x>width){this.pos.x = width}
+        if(this.pos.y>height){this.pos.y = height}
     }
 
     this.eats = function(other) {
         var d = p5.Vector.dist(this.pos, other.pos);
         if (d < this.r + other.r){
             var sum = PI * this.r * this.r + PI *other.r * other.r;
-            this.r = sqrt(sum / PI);
+            if(this.r < 100) this.r = sqrt(sum / PI)
 
-            // this.r +=other.r*0.2;
             return true;
         } else {
             return false;
@@ -23,7 +27,7 @@ function Blob(x, y, r) {
     }
 
     this.show = function(){
-        fill(255, 255, 100);
+
         ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
     }
 
--- my/Agar.io/js/script.js
+++ my/Agar.io/js/script.js
@@ -1,114 +1,53 @@
-var blob;
-var blobs = [];
-var zoom = 1;
+let blob
+let blobs = []
+let zoom = 1
 
-var spacing = 10
-var k = 30
-var w = spacing / Math.sqrt(2)
-var grid = []
-var active = []
-var cols
-var rows
-var food = []
+function restoreFood(){
+  let n = new Blob(random(-width, width), random(-height, height), 16)
+  blobs.push(n)
+}
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(600, 600)
   strokeWeight(4)
-  cols = floor(width / w)
-  rows = floor(height / w)
+  fill(random(255), random(255), random(255))
 
-  // step 0
-  for (let i = 0; i < cols * rows; i++) {
-    grid[i] = undefined
-  }
-
-  // step 1
-  let x = random(width)
-  let y = random(height)
-  let i = floor(x / w)
-  let j = floor(y / w)
-  let pos = createVector(x, y)
-  grid[i + j * cols] = pos
-  active.push(pos)
-
-
-  while (active.length > 0) {
-    let randIndex = floor(random(active.lenght))
-    let pos = active[randIndex]
-    let found = false
-    for (let n = 0; n < k; n++) {
-      let sample = p5.Vector.random2D()
-      let m = random(spacing, 2 * r)
-      sample.setMag(m)
-      sample.add(pos)
-
-      var col = floor(sample.x / w)
-      var row = floor(sample.y / w)
-      if (col > -1 && row > -1 && col < cols && row < rows && !grid[col + row * cols]) {
-        var ok = true
-        for (let i = -1; i <= 1; i++) {
-          for (let j = -1; j <= 1; j++) {
-            var index = (col + i) + (row + j) * cols
-            var neighbor = grid[index]
-            if (neighbor) {
-              var d = p5.Vector.dist(sample, neighbor)
-              if (d < spacing) {
-                ok = false
-              }
-            }
-          }
-        }
-        if (ok) {
-          found = true
-          grid[col + row * cols] = sample
-          active.push(sample)
-          break
-        }
-      }
-    }
-
-    if (!found) {
-      active.splice(randIndex, 1)
-    }
-  }
-
-  for (g of grid) {
-    if (g) {
-      food.push(createVector(g.x, g.y))
-    }
-  }
-
-  blob = new Blob(0, 0, 64);
+  blob = new Blob(0, 0, 64)
 
   for (let i = 0; i < 50; i++) {
-    blobs[i] = new Blob(food[i].x, food[i].y, 16);
+    blobs[i] = new Blob(random(-width, width), random(-height, height), 16)
   }
 }
 
 function draw() {
+  print(blob.r)
   background(0);
-  translate(width / 2, height / 2);
-  var newzoom = 64 / blob.r;
-  zoom = lerp(zoom, newzoom, 0.1);
-  scale(zoom);
-  translate(-blob.pos.x, -blob.pos.y);
-  for (let i = -height; i <= height; i = i + 50) {
+  translate(width / 2, height / 2)
+  var newzoom = 64 / blob.r
+  zoom = lerp(zoom, newzoom, 0.1)
+  scale(zoom)
+  translate(-blob.pos.x, -blob.pos.y)
+  for (let i = -height*2; i <= height*2; i = i + 50) {
     stroke(100)
     strokeWeight(1)
-    line(-width, i, width, i)
+    line(-width*2, i, width*2, i)
   }
-  for (let i = -width; i <= width; i = i + 50) {
+  for (let i = -width*2; i <= width*2; i = i + 50) {
     stroke(100)
     strokeWeight(1)
-    line(i, -height, i, height)
+    line(i, -height*2, i, height*2)
   }
 
   for (var i = blobs.length - 1; i >= 0; i--) {
-    blobs[i].show();
+
+
+    blobs[i].show()
     if (blob.eats(blobs[i])) {
-      blobs.splice(i, 1);
+      blobs.splice(i, 1)
+      setTimeout(restoreFood, 200)
     }
   }
-  blob.show();
-  blob.update();
+
+  blob.show()
+  blob.update()
 }
