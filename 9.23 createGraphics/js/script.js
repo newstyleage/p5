fd820847606507edc31ead5aeb01fc23ac4a9bc8
NS <newstyleage@gmail.com>
2017-12-30 06:26:53 +0100

    9.23 draw with mouse

--- 9.23 createGraphics/js/script.js
+++ 9.23 createGraphics/js/script.js
@@ -1,24 +1,23 @@
+let graphics
+
 function setup() {
-  createCanvas(480, 360);
-  angleMode(DEGREES);
+  pixelDensity(1)
+  createCanvas(400, 300);
+  graphics = createGraphics(400, 300)
+  graphics.background(51);
+
 }
 
 function draw() {
-  background(51);
-  stroke(255, 255, 100);
-  noFill();
-  strokeWeight(4);
-  point(mouseX, mouseY);
-  point(150, 50);
-  point(250, 60);
-  point(300, 200);
-  strokeWeight(1);
-  beginShape();
+  if (mouseIsPressed) {
+    graphics.fill(255, 255, 100)
+    graphics.stroke(255, 255, 100)
+    graphics.ellipse(mouseX, mouseY, 64)
+  }
 
-    curveVertex(mouseX,mouseY);
-    curveVertex(150,50);
-    curveVertex(250,60);
-    curveVertex(300,200);
+  image(graphics, 0, 0)
 
-  endShape(CLOSE);
+  noFill()
+  ellipse(mouseX, mouseY, 64)
+
 }
