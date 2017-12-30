6ece9e6d7d2238904246f24b1d36918e7c2cfdfe
NS <newstyleage@gmail.com>
2017-12-30 06:39:53 +0100

    9.23 duplicating

--- 9.23 createGraphics/js/script.js
+++ 9.23 createGraphics/js/script.js
@@ -1,23 +1,34 @@
 let graphics
+let x = 50
+let y = 50
+let angle = 0
 
 function setup() {
   pixelDensity(1)
   createCanvas(400, 300);
-  graphics = createGraphics(400, 300)
-  graphics.background(51);
+  graphics = createGraphics(100, 100)
+  // graphics.background(51);
 
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    graphics.fill(255, 255, 100)
-    graphics.stroke(255, 255, 100)
-    graphics.ellipse(mouseX, mouseY, 64)
-  }
+  background(0)
+  graphics.fill(255, 255, 100)
+  graphics.stroke(255, 255, 100)
+  graphics.ellipse(x, y, 10)
+  x += random(-5, 5)
+  y += random(-5, 5)
 
+  imageMode(CENTER)
+  image(graphics, mouseX, mouseY)
+
+  push()
+  translate(200, 200)
+  tint(0, 255, 0)
+  rotate(angle)
   image(graphics, 0, 0)
+pop()
 
-  noFill()
-  ellipse(mouseX, mouseY, 64)
+angle +=0.1
 
 }
