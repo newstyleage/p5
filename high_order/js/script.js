d4d254ef26bcf2b04767af75e72fbf397e223279
NS <newstyleage@gmail.com>
2018-02-17 21:45:23 +0100

    map fill high order

---
+++ high_order/js/script.js
@@ -0,0 +1,49 @@
+// c+s+p quokka
+let arr= [4,8,1,2,9]
+console.log(arr)
+
+// function doubler(x) {
+//   return x*2
+// }
+// console.log(arr.map(doubler))
+console.log(arr.map(x=>x*2))
+
+let rand = new Array(10)
+console.log(rand.fill(0).map(Math.random))
+
+
+// function multiplier(factor) {
+//   return  x => x* factor
+// }
+
+// let multiplier = factor => x => x*factor
+
+// // function multiplier(factor) {
+// //   return function(x) {
+// //     return x * factor
+// //   }
+// // }
+
+// let doubler = multiplier(2);
+// let tripler = multiplier(3);
+
+// function sing(callback) {
+//   console.log("la la la")
+//   if (callback instanceof Function) callback();
+// }
+
+// function meow(){
+//   console.log("meow meow")
+// }
+
+
+
+
+
+
+function setup() {
+}
+
+function draw() {
+
+}
