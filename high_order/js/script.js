39533866692f392fd90d98089b36a9f143a90422
NS <newstyleage@gmail.com>
2018-02-19 09:05:05 +0100

    sort filter

--- high_order/js/script.js
+++ high_order/js/script.js
@@ -26,6 +26,43 @@ console.log(arr.reduce((acc, val) => {
 }))
 console.log(arr.reduce((a, v) => a < v ? a : v))
 
+console.log(arr.filter(x => x % 2))
+console.log(arr.filter(x => !(x % 2)))
+
+const s = "It was a dark and stormy  night 65."
+let w = s.split(' ')
+console.log(w)
+console.log(s.split(/\W+/).filter(s => s))
+console.log(s.split(/\W+/).filter(s => s.length >= 3))
+
+console.log(arr.sort())
+console.log(w.sort())
+console.log(w.sort((a, b) => a.length - b.length))
+const obj = [{
+    x: 5,
+    y: 6
+  },
+  {
+    x: 9,
+    y: 44
+  },
+  {
+    x: 11,
+    y: 22
+  },
+  {
+    x: 44,
+    y: 4
+  },
+  {
+    x: 3,
+    y: 33
+  }
+]
+console.log(obj)
+console.log(obj.sort())
+console.log(obj.sort((a, b) => a.y - b.y))
+
 // function multiplier(factor) {
 //   return  x => x* factor
 // }
@@ -49,14 +86,3 @@ console.log(arr.reduce((a, v) => a < v ? a : v))
 // function meow(){
 //   console.log("meow meow")
 // }
-
-
-
-
-
-
-function setup() {}
-
-function draw() {
-
-}
