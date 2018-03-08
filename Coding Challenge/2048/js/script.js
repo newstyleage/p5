948cb07a38b69b9689a083b04d56559a18969908
NS <newstyleage@gmail.com>
2018-03-08 17:35:34 +0100

    2048 started....

---
+++ Coding Challenge/2048/js/script.js
@@ -0,0 +1,19 @@
+let grid
+function setup(){
+  createCanvas(400, 400)
+  grid = new Array(4)
+  grid = grid.fill().map(x=>[0,0,0,0])
+  console.table(grid)
+}
+
+function addNumber() {
+  let options = []
+  for (o of options) {
+
+  }
+}
+
+
+function draw(){
+  background(255,200,0)
+}
