f4875d5383266111f2267eb3d698ea36b38572e7
NS <newstyleage@gmail.com>
2018-03-08 22:22:11 +0100

    CC 86 finished

--- Coding Challenge/86 Cube Wave by Bees and Bombs/js/script.js
+++ Coding Challenge/86 Cube Wave by Bees and Bombs/js/script.js
@@ -1,21 +1,40 @@
 let angle = 0;
+let w = 24;
+let ma;
+let maxD;
 
-function setup(){
-  createCanvas(400, 400);
+function setup() {
+  createCanvas(400, 400, WEBGL);
+  ma = atan(1 / sqrt(2))
+  maxD = dist(0, 0, 200, 200);
 }
 
-function draw(){
+function draw() {
   background(0);
-  translate(width/2, height/2);
-  rectMode(CENTER);
+  ortho(-400, 400, -400, 400, 0, 800);
+  // translate(0, 50, -200);
+  rotateX(-QUARTER_PI + 0.3);
+  rotateY(ma);
+  // rotateX(PI / 4);
+  // rotateX(angle/4);
+  // translate(width/2, height/2);
+
   let offset = 0;
-  for (let x = 0; x < width; x+=50){
-    let a = angle + offset;
-    let h = map(sin(a), -1, 1, 0, 100);
-    rect(x - width/2, 0, 9, h);
-    offset += 0.1;
+  for (let z = 0; z < height; z += w) {
+    for (let x = 0; x < width; x += w) {
+      push();
+      let d = dist(x, z, width / 2, height / 2)
+      offset = map(d, 0, maxD, -PI, PI);
+      let a = angle + offset;
+      let h = floor(map(sin(a), -1, 1, 50, 300));
+      translate(x - width / 2, 0, z - height / 2);
+      normalMaterial();
+      // ambientMaterial(250);
+      box(w - 2, h, w - 2);
+      // rect(x - width/2 + w / 2, 0, w - 2, h);
+      pop();
+    }
+    // offset += 0.1;
   }
-  angle += 0.1;
-
-
+  angle -= 0.14;
 }
