e9b34c070614343835f7a2d566593ca7bdc29d3f
NS <newstyleage@gmail.com>
2018-03-08 21:02:36 +0100

    CC 86 almost start

---
+++ Coding Challenge/86 Cube Wave by Bees and Bombs/js/script.js
@@ -0,0 +1,21 @@
+let angle = 0;
+
+function setup(){
+  createCanvas(400, 400);
+}
+
+function draw(){
+  background(0);
+  translate(width/2, height/2);
+  rectMode(CENTER);
+  let offset = 0;
+  for (let x = 0; x < width; x+=50){
+    let a = angle + offset;
+    let h = map(sin(a), -1, 1, 0, 100);
+    rect(x - width/2, 0, 9, h);
+    offset += 0.1;
+  }
+  angle += 0.1;
+
+
+}
